9f46bc9aaf9e2544726f95e7499858aa4138d73e
yotiosoft <ytani0323@gmail.com>
2021-06-05 18:29:52 +0900

    クリック判定テスト

--- js/img_transparent.js
+++ js/img_transparent.js
@@ -16,3 +16,10 @@ function previewFile(file) {
     // ファイル読み込み
     render.readAsDataURL(file);
 }
+
+document.getElementById('preview').onclick = function(event) {
+	var x = event.pageX ;	// 水平の位置座標
+	var y = event.pageY ;	// 垂直の位置座標
+
+    document.write(x+","+y);
+}
